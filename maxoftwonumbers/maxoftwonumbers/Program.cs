7ad7d977f3aabd25b7ece36b8aafb4531c7fc050
jmockbee <mockbee1000@gmail.com>
2018-06-18 13:00:58 -0400

    the computer will display the larger of the two numbers

--- maxoftwonumbers/maxoftwonumbers/Program.cs
+++ maxoftwonumbers/maxoftwonumbers/Program.cs
@@ -10,6 +10,16 @@ namespace maxoftwonumbers
     {
         static void Main(string[] args)
         {
+           
+                Console.Write("Enter a number: ");
+                var number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter another number: ");
+                var number2 = Convert.ToInt32(Console.ReadLine());
+
+                var max = (number1 > number2) ? number1 : number2;
+                Console.WriteLine("Max is " + max);
+            
         }
     }
 }
